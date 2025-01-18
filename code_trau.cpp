673eb1cd868062ab100cd14f74edd4e7d3e3fa53
NhwBach_Tik25 <160082744+NhwBachTik25@users.noreply.github.com>
2025-01-19 00:06:27 +0700

    Update code_trau.cpp

--- code_trau.cpp
+++ code_trau.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <algorithm>  
-#include <utility>    
+#include <bits/stdc++.h>   
 #define int long long
 #define fu(i,a,b) for(int i=a;i<=b;i++)
 #define fd(i,a,b) for(int i=a;i>=b;i--)
